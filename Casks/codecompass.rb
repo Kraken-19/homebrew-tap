4a6e9c22bb538dc81fc3b61ec075e7dce8cd9150
kraken <138370010+Kraken-19@users.noreply.github.com>
2026-09-06 13:47:48 +0000

    chore: update CodeCompass to 0.2.0

--- Casks/codecompass.rb
+++ Casks/codecompass.rb
@@ -1,6 +1,6 @@
 cask "codecompass" do
-  version "0.1.4"
-  sha256 "bfa6089371dc58a20b07b94a022083e24d86af4185e01e0d8a136028ba411143"
+  version "0.2.0"
+  sha256 "442132df5cd783d243663380f0e3a77834d7f6a0222b371114d4745f937ddd96"
 
   url "https://omneon-storage.fsn1.your-objectstorage.com/releases/codecompass/#{version}/CodeCompass-#{version}.dmg"
   name "CodeCompass"
